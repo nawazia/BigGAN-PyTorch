fab97ba855f72f3bdf700412e0142c89bd47e1b6
nawazia <ibrahim.nawaz1@icloud.com>
2022-02-21 17:16:18 +0000

    num_workers = 4

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python make_hdf5.py --dataset I128 --batch_size 256 --data_root data
-python calculate_inception_moments.py --dataset real2 --data_root '/content/drive/My Drive'
+python calculate_inception_moments.py --dataset real2 --data_root '/content/drive/My Drive' --num_workers 4
