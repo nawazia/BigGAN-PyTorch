4d3dc8c62b24c7776ac16d3ffd2ccd3e35fe19b3
nawazia <ibrahim.nawaz1@icloud.com>
2022-04-19 01:52:46 +0100

    added size comment (also -debug last update I think)

--- train.py
+++ train.py
@@ -170,8 +170,7 @@ def run(config):
       pbar = tqdm(loaders[0])
     for i, (x, y) in enumerate(pbar):
       # Increment the iteration counter
-      x = x.squeeze(0)
-#       y = y.squeeze()
+      x = x.squeeze(0)      #   (16,3,64,64)
       state_dict['itr'] += 1
 #       print(x.size(),y.size())
       # Make sure G and D are in training mode, just in case they got set to eval
