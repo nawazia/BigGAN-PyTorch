bd462dff7e14935e2faa055dc4d6a500f59c82c5
nawazia <ibrahim.nawaz1@icloud.com>
2022-04-22 01:26:20 +0100

    cgan

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python make_hdf5.py --dataset I128 --batch_size 256 --data_root data
-python calculate_inception_moments.py --dataset real2 --data_root '/content/drive/My Drive' --num_workers 4
+python calculate_inception_moments.py --dataset cgan --data_root '/content/drive/My Drive' --num_workers 4
